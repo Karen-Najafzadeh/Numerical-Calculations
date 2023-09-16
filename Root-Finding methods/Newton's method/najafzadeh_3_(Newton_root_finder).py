c0a0836ba202a6c7aa368b5d8fc85111b4862b81
Karen <najafzadehkaren@gmail.com>
2023-09-16 16:07:55 +0330

    newton method done

--- Root-Finding methods/Newton's method/najafzadeh_3_(Newton_root_finder).py
+++ Root-Finding methods/Newton's method/najafzadeh_3_(Newton_root_finder).py
@@ -1,27 +1,23 @@
-#a program to find the root of a mathematical function using Newton-Raphson method.
-# mohammad sadegh najafzadeh
-# 982042031
 #importing essentials
 import sympy as sp
 
 #definitions
 x = sp.symbols("x")
-eps = float(input("how accurate?\n"))
+precision = float(input("how accurate?\n"))
 x_i = float(input("give me a random x\n"))
 
 def f(x):
     return x**2
+
 m = sp.diff(f(x),x)
 
 #algorithm
 
-with open("najafzadeh_3_(Newton_root_finder).txt",'w') as file:
-    intro = "a program to find the root of a mathematical function using Newton-Raphson method.\nmohammad sadegh najafzadeh\n982042031\n"
-    file.write(intro)
-    while True:
-        x_new = ((-f(x_i))/(m.subs(x,x_i)))+x_i
-        if abs(f(x_new))<=eps:
-            file.write(f"\nroot is {x_new}\n")
-            break
-        else:
-            x_i = x_new
+
+while True:
+    x_new = ((-f(x_i))/(m.subs(x,x_i)))+x_i
+    if abs(f(x_new))<= precision:
+        print(f"\n{x_new} is one of the possible roots\n")
+        break
+    else:
+        x_i = x_new
