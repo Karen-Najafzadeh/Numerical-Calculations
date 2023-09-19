5dd4c0f3c193e63b12a5d3ccf70fd774c71d7016
Karen <najafzadehkaren@gmail.com>
2023-09-19 16:08:25 +0330

    made some changes

--- Integration methods/Trapezoid method/Trapezoid_integral.py
+++ Integration methods/Trapezoid method/Trapezoid_integral.py
@@ -10,13 +10,14 @@ h = float(input('how accurate? (less number = more accuracy)\n'))
 def f(x):
     return ((x-1)**2)/((math.e)**x)
 sum = 0
-iterations = (b-a)/h
+iterations = int((b-a)/h)
 
 # the actual algorithm
-for i in range(1,int(iterations)+1):
+# we start the iteration from 1 because we don't want to include the first and the last trapezoid in the calculation
+for i in range(1,iterations):
     sum += 2*f(a+(h*i))
 
-integral = (h/2)*(sum-f(a)-f(b))
+integral = (h/2)*(sum+f(a)+f(b))
 
 #result
 print(f"integral of currunt mathematical function is {integral}")
