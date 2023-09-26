f9d4f8fdb71dd233188474843daeb2e539961718
Karen <najafzadehkaren@gmail.com>
2023-09-26 23:25:28 +0330

    Update fitting_k_square.py

--- Fitting/Least square method/fitting_k_square.py
+++ Fitting/Least square method/fitting_k_square.py
@@ -1,28 +1,5 @@
-def Library_Installer():
-    #if any problems show up when we try to import a library, this will fix the problem
-    import os #anyone have this library by default, so there won't be any problems here
-
-    # finding user's desktop directory path
-    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
-
-    #creating a .bat file and writing the cmd commands that you wnat
-    with open (f"{desktop}\my_bat.bat",'w') as file:
-        file.write(f'pip install numpy')
-        file.write(f'pip install matplotlib')
-
-    # running the .bat file
-    os.system(f"{desktop}/my_bat.bat")
-
-    # deleting the .bat file after we are finished installing
-    os.remove(f"{desktop}\my_bat.bat")
-
-# here we go, start of program
-try:
-    import matplotlib.pyplot as plt
-    import numpy as np
-except:
-    Library_Installer()
-
+import matplotlib.pyplot as plt
+import numpy as np
 # taking input from user
 n = int(input("how many numbers you have?\n"))
 x = [float(input(f"what is the x Nunmer{i+1}?\n")) for i in range(n)]  # for example =  [1,3,4,6,8,9,11,14]
